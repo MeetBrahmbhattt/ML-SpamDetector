2b7eeb3ca5547d1a01075228be64121cf16a5e2f
MeetBrahmbhattt <meet.brahmbhatt10l@gmail.com>
2023-03-24 12:31:08 +0530

    updated training part

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
@@ -20,21 +19,3 @@ from sklearn.naive_bayes import MultinomialNB
 naive_bayes = MultinomialNB()
 naive_bayes.fit(X_train,y_train)
 naive_bayes.score(X_test,y_test)
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-	if request.method == 'POST':
-		comment = request.form['comment']
-		data = [comment]
-		vect = cv.transform(data).toarray()
-		my_prediction = naive_bayes.predict(vect)
-	return render_template('result.html',prediction = my_prediction)
-
-if __name__ == '__main__':
-	app.run(debug=True)
